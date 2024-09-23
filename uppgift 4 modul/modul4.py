9d163c51c9d5da581b4eba4e4852886a9e3b5dee
StandardBuster <trbeho2007@gmail.com>
2024-09-23 14:30:55 +0200

    modul 4

---
+++ uppgift 4 modul/modul4.py
@@ -0,0 +1,98 @@
+import os
+os.system("cls")
+
+
+dingus = ["Shabbo", "Jimbo", "Dingo"]
+for x in dingus:
+    print(x)
+    if x == "Jimbo":
+        continue
+    print(x)
+
+
+if 7 > 4:
+    print("Seven is greater than 4! ")
+
+
+
+count = 0
+
+while count < 10:
+    count += 1
+
+    print(str(count) + ") Item Stand")
+
+
+
+for i in range(1, 10):
+    print(i)
+
+
+name = ["MY NINJA!!!! TO THE CLIFFS IT IS!"]
+
+for letter in name:
+    print(letter)
+
+
+numbers = [3,6,5,2]
+
+print(numbers)
+
+
+for number in numbers:
+    print(number)
+
+
+for table in range(1, 13):
+    for x in range(1, 11):
+        print(f"{table} * {x} = {table * x}")
+
+
+
+
+
+
+name = "Naijse"
+age = 17
+print(f"Hey {name}, you are {age} years old")
+
+
+
+
+special_simulator = ""
+
+
+while special_simulator != "q":
+    for i in range(69,420):
+        print("HAHAHA YOU'RE STUCK!!!!!")
+    special_simulator = input("You're DONE buddy! ")
+
+intelligent_word = input("What do you want dude? ")
+for i in range(5, 40):
+    print(intelligent_word)
+
+
+
+while True:
+    letter = input("Ddududududu Q to exit ").upper()
+
+    if letter == "Q":
+        break
+    else:
+        print(letter)
+
+
+
+for numero in range(2):
+    for n in range(1, 10):
+        print(f"{numero} + {n} = {numero + n}")
+
+
+repetition_variabel = int(input("Put in your numbers! "))
+
+for siffra in range(1, repetition_variabel + 1):
+    print(siffra)
+
+
+
+
